619051d70503608d5629431c82819a051f1fff58
KelvinNHC <huapayita.2001@gmail.com>
2023-01-11 14:33:12 -0500

    Pantalla registrar citas y listar citas

---
+++ app/src/main/java/com/app/proyecto_citamedica/ListarCitaActivity.java
@@ -0,0 +1,14 @@
+package com.app.proyecto_citamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarCitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_cita);
+    }
+}
---
+++ app/src/main/java/com/app/proyecto_citamedica/NuevaCitaActivity.java
@@ -0,0 +1,14 @@
+package com.app.proyecto_citamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaCitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_cita);
+    }
+}
