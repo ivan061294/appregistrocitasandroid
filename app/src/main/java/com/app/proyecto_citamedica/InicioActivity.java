1076a3c1782fafff5d9aa721d6b2624b70e77045
ivan061294 <ivangasparquispe61294@gmail.com>
2023-01-11 21:36:58 -0500

    cambios en dev agregado de fragment citas

---
+++ app/src/main/java/com/app/proyecto_citamedica/InicioActivity.java
@@ -0,0 +1,144 @@
+package com.app.proyecto_citamedica;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+
+
+import com.app.proyecto_citamedica.databinding.ActivityInicioBinding;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.badge.BadgeUtils;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class InicioActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityInicioBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityInicioBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarInicio.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_inicio, R.id.nav_list_citas, R.id.nav_configuracion)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_inicio);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.inicio, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.cerrarSesion:
+                this.logout();
+                break;
+            case R.id.bolsaCompras:
+                //this.mostrarBolsa();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*private void mostrarBolsa() {
+        Intent i = new Intent(this, PlatillosCarritoActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }*/
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadData();
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private void loadData() {
+       /* SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        final Gson g = new GsonBuilder()
+                .registerTypeAdapter(Date.class, new DateSerializer())
+                .registerTypeAdapter(Time.class, new TimeSerializer())
+                .create();
+        String usuarioJson = sp.getString("UsuarioJson", null);
+        if(usuarioJson != null){
+            final Usuario u = g.fromJson(usuarioJson, Usuario.class);
+            final View vistaHeader = binding.navView.getHeaderView(0);
+            final TextView tvNombre = vistaHeader.findViewById(R.id.tvNombre),
+                    tvCorreo = vistaHeader.findViewById(R.id.tvCorreo);
+            final CircleImageView imgFoto = vistaHeader.findViewById(R.id.imgFotoPerfil);
+            tvNombre.setText(u.getCliente().getNombreCompleto());
+            tvCorreo.setText(u.getEmail());
+            String url = ConfigApi.baseUrlE + "/api/documento-almacenado/download/" + u.getCliente().getFoto().getFileName();
+            final Picasso picasso = new Picasso.Builder(this)
+                    .downloader(new OkHttp3Downloader(ConfigApi.getClient()))
+                    .build();
+            picasso.load(url)
+                    .error(R.drawable.image_not_found)
+                    .into(imgFoto);
+        }
+        BadgeDrawable badgeDrawable = BadgeDrawable.create(this);
+        badgeDrawable.setNumber(Carrito.getDetallePedidos().size());
+        BadgeUtils.attachBadgeDrawable(badgeDrawable, findViewById(R.id.toolbar), R.id.bolsaCompras);*/
+    }
+
+    //Método para cerrar sesión
+    private void logout() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(InicioActivity.this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("preferenciasLogin");
+        editor.apply();
+        this.finish();
+        this.overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_inicio);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
